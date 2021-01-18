35a7c8410603fadd1e7b9dbbad482dac89382c6c
Jitesh Singh <jitust98@gmail.com>
2021-01-18 13:27:02 +0530

    Basic Two fragments Replacing MainActivity

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -1,14 +1,43 @@
 package com.androidbasics;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1;
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1=findViewById(R.id.tab1);
+      btn2=  findViewById(R.id.tab2);
+
+      btn1.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              FragmentManager fragmentManager=  getSupportFragmentManager();
+              FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+              fragmentTransaction.add(R.id.wrapper,new Tab1());
+              fragmentTransaction.addToBackStack(null);
+              fragmentTransaction.commit();
+          }
+      });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager=  getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+                fragmentTransaction.add(R.id.wrapper,new Tab2());
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
     }
 }
