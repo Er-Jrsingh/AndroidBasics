bf7c2eab86dd5dff30a3caae32d8943fb340e8f7
Jitesh Singh <jitust98@gmail.com>
2021-01-30 22:12:40 +0530

    Fetch Network Image Using Glide Library

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -3,12 +3,19 @@ package com.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
-
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img=findViewById(R.id.img);
+
+        Glide.with(this).load("https://cdn.pixabay.com/photo/2016/03/27/19/20/indian-1283789_1280.jpg").into(img);
     }
 }
