320bae3046d41f4e62548caaf4c514e3c54911d9
Jitesh Singh <jitust98@gmail.com>
2021-02-05 20:59:00 +0530

    Carousel Using Inbuilt Methods

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -3,12 +3,32 @@ package com.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+    ViewFlipper flipper=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] imgArray ={R.drawable.slide1,R.drawable.slide2,R.drawable.slide3,R.drawable.slide4,R.drawable.slide5};
+        flipper= findViewById(R.id.flipper);
+        for (int value : imgArray) {
+            Carousel(value);
+        }
+
+    }
+    public  void  Carousel(int img){
+        ImageView imageView= new ImageView(this);
+        imageView.setBackgroundResource(img);
+        flipper.addView(imageView);
+        flipper.setFlipInterval(3000);
+        flipper.setAutoStart(true);
+        flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        flipper.setOutAnimation(this,android.R.anim.slide_out_right);
+
     }
 }
