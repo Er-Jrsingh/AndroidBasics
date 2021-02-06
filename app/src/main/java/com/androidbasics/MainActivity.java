3f83944a983544c483f328be4f2ab52c97a5f2cb
Jitesh Singh <jitust98@gmail.com>
2021-02-06 17:55:02 +0530

    Customizing Progress Dialog Without External Library

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -2,13 +2,29 @@ package com.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button prodia = findViewById(R.id.prodia);
+        prodia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.show();
+                progressDialog.setContentView(R.layout.progress_bar);
+                progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            }
+
+        });
     }
 }
