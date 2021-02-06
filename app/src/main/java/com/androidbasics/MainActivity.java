cf1c0ca05a428c45262fe62490e92f56740e5173
Jitesh Singh <jitust98@gmail.com>
2021-02-06 18:15:59 +0530

    Toast Method 1

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -3,6 +3,12 @@ package com.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.lang.reflect.Method;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button toasty=findViewById(R.id.toasty);
+        toasty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*             Method 1         */
+
+                Toast.makeText(getApplicationContext(),"Hola!! Toast",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
 }
