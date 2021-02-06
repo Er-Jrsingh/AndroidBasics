c2391b90b53ada69cd80e84f11fa805175baa85a
Jitesh Singh <jitust98@gmail.com>
2021-02-06 22:44:01 +0530

    Hide Action Bar Using Java Code

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
         // Hide Status Bar
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
+        //Hide Action Bar
+        getSupportActionBar().hide();
     }
 }
