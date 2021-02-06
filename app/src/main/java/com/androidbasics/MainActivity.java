daaac095cd7071d6103e9356e5e7e7f3eb857561
Jitesh Singh <jitust98@gmail.com>
2021-02-06 20:56:10 +0530

    Share data Outside The App Using Intent

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -2,13 +2,67 @@ package com.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.input);
+        textView = findViewById(R.id.txtVw);
+        Button share = findViewById(R.id.share);
+        Button button = findViewById(R.id.btnI);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+                if (name.isEmpty()) {
+                    alert();
+                } else {
+                    textView.setText(name);
+                }
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = editText.getText().toString();
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT, name);
+                startActivity(sendIntent);
+            }
+        });
+    }
+    void alert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+        alertDialog.setTitle("You Entered Nothing");
+        alertDialog.setMessage("Enter Your Credentials First");
+        alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        })
+                .create();
+        alertDialog.show();
     }
 }
