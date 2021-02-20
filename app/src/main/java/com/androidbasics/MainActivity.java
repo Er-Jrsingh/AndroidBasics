24548ef7c1d7414cb2a98c93cec7361f518e68e4
Jitesh Singh <jitust98@gmail.com>
2021-02-20 17:35:07 +0530

    This Not Eligible Because Inflater Must Associate with Any Application Context

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -2,7 +2,14 @@ package com.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button add=findViewById(R.id.add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               LayoutInflater layoutInflater=new LayoutInflater() {
+                   @Override
+                   public LayoutInflater cloneInContext(Context newContext) {
+                       return null;
+                   }
+               }
+            }
+        });
     }
 }
