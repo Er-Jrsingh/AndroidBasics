5fb1389ccd626cfb23c134b715abe8b1a03814e6
Jitesh Singh <jitust98@gmail.com>
2021-02-21 16:03:21 +0530

    Add Snackbar

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -2,7 +2,12 @@ package com.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void Snacky(View view) {
+       Snackbar snackbar= Snackbar.make(view,"Hola !! Snacky",5000);
+
+       snackbar.setAction("undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Successfully Undo",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        snackbar.setTextColor(Color.GREEN);
+        snackbar.setBackgroundTint(Color.BLACK);
+        snackbar.show();
     }
 }
