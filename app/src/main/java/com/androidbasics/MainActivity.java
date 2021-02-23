0517209b88bc47c4e66556fbc1a72a1983795af1
Jitesh Singh <jitust98@gmail.com>
2021-02-23 13:23:43 +0530

    Add User Actions On AlertDialog

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.DialogCompat;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +21,29 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder alertBuilder= new AlertDialog.Builder(this);
         alertBuilder.setTitle("Confirmation Box");
         alertBuilder.setMessage("Are You Sure??");
-        alertBuilder.setCancelable(true);
+        alertBuilder.setCancelable(false);
+
+        // Add User Actions On AlertDialog
+        alertBuilder.setPositiveButton("Yup", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Ohh Yesssss!!...",Toast.LENGTH_LONG).show();
+            }
+        });
+        alertBuilder.setNegativeButton("Nooop", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"So Sorry !!...",Toast.LENGTH_LONG).show();
+            }
+        });
+        alertBuilder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Thik Hai Bhai",Toast.LENGTH_LONG).show();
+                alertBuilder.create().dismiss();
+            }
+        });
+
         alertBuilder.show();
 
     }
