04cc80254eddf365d1f4c522245ba0c5639e7397
Jitesh Singh <jitust98@gmail.com>
2021-02-23 13:09:57 +0530

    Create AlertDialog

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -1,8 +1,11 @@
 package com.androidbasics;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.DialogCompat;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void createDialog(View view) {
+        AlertDialog.Builder alertBuilder= new AlertDialog.Builder(this);
+        alertBuilder.setTitle("Confirmation Box");
+        alertBuilder.setMessage("Are You Sure??");
+        alertBuilder.setCancelable(true);
+        alertBuilder.show();
+
     }
 }
