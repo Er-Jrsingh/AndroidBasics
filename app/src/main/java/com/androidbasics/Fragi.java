6edd66b82ca886afadebeff2b430afc125f57105
Jitesh Singh <jitust98@gmail.com>
2021-02-26 12:16:07 +0530

    Fragment By Xml (Used For Static Display )

--- app/src/main/java/com/androidbasics/Fragi.java
+++ app/src/main/java/com/androidbasics/Fragi.java
@@ -8,19 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class Tab1 extends Fragment {
+public class Fragi extends Fragment {
 
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        return inflater.inflate(R.layout.fragi_1, container, false);
     }
 }
--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -10,35 +10,10 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
-    Button tab1;
-    Button tab2;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tab1 = findViewById(R.id.tab1);
-        tab2 = findViewById(R.id.tab2);
-
-        tab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Tab1 tb1 = new Tab1();
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.wrapper, tb1);
-                transaction.commit();
-            }
-        });
-
-        tab2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Tab2 aboutFragment = new Tab2();
-                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                transaction.replace(R.id.wrapper, aboutFragment);
-                transaction.commit();
-            }
-        });
 
     }
 }
