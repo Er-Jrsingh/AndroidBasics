57f3194cc3e84de35dc64f569568fb2cc9751217
Jitesh Singh <jitust98@gmail.com>
2021-02-26 12:39:49 +0530

    Fragment By Java (Used For Dynamic Display ),With Bug (remove btn  Not work after Orientation Change )

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -2,18 +2,32 @@ package com.androidbasics;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
+    Fragi fragi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void showFragi(View view) {
+        fragi = new Fragi();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragi_container,fragi)
+                .commit();
+    }
+
+    public void removeFragi(View view) {
+        if (fragi != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .remove(fragi)
+                    .commit();
+        }
     }
 }
