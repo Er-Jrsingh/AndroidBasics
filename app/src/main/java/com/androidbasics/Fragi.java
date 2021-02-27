50a7ea5ad61c223761c2b38730cb096ddeefabd5
Jitesh Singh <jitust98@gmail.com>
2021-02-27 11:56:35 +0530

    Share Data From Parent Activity to Fragment Using Custom Method(setData())(Advantage We can Also Share Non Primitive Data  )

--- app/src/main/java/com/androidbasics/Fragi.java
+++ app/src/main/java/com/androidbasics/Fragi.java
@@ -1,5 +1,6 @@
 package com.androidbasics;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 public class Fragi extends Fragment {
+    private String name;
 
 
 //    @Override
@@ -18,19 +20,26 @@ public class Fragi extends Fragment {
 //
 //    }
 
+    @SuppressLint("SetTextI18n")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragi_1, container, false);
-        Bundle bundle=getArguments();
-        if (bundle!= null){
-            String name= bundle.getString("key","name missing");
-            TextView textView=view.findViewById(R.id.fragi_tv);
-            textView.setText("Hola ! ! "+ name+" I am Fragment");
-        }
 
+//        Bundle bundle=getArguments();
+//        if (bundle!= null){
+//            String name= bundle.getString("key","name missing");
+//            TextView textView=view.findViewById(R.id.fragi_tv);
+//            textView.setText("Hola ! ! "+ name+" I am Fragment");
+//        }
+
+        TextView textView=view.findViewById(R.id.fragi_tv);
+        textView.setText("Hola ! ! "+ name+" I am Fragment");
 
         return view;
+    }
+    public  void  setData(String name){
+        this.name=name;
     }
 }
--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -7,28 +7,26 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     EditText editText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = findViewById(R.id.et_name);
     }
 
-
     public void showFragi(View view) {
-        String name= editText.getText().toString();
-        Bundle bundle=new Bundle();
-        bundle.putString("key",name);
+        String name = editText.getText().toString();
+
+//        Bundle bundle=new Bundle();
+//        bundle.putString("key",name);
 
         Fragi fragi = new Fragi();
-        fragi.setArguments(bundle);
+//        fragi.setArguments(bundle);
+        fragi.setData(name);
         getSupportFragmentManager()
                 .beginTransaction()
-                .add(R.id.fragi_container,fragi,"frag_tag")
+                .add(R.id.fragi_container, fragi, "frag_tag")
                 .commit();
     }
 
