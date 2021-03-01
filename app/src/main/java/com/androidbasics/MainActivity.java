92249491499194ef556f7d215ace9977292bd34b
Jitesh Singh <jitust98@gmail.com>
2021-03-01 13:04:56 +0530

    Measuring Device Screen Size Using Java Code & Take Decision Use 2 Fragments Side By Side Or not

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -6,10 +6,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mShowSize;
+    private boolean mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
     public void showSize(View view) {
         ScreenUtils screenUtils = new ScreenUtils(this);
         mShowSize.setText(String.format("Width: %s , Height: %s", screenUtils.getHorizontalWidth(), screenUtils.getVerticalHeight()));
+
+        if (screenUtils.getHorizontalWidth()>=620){
+            mFragment=true;
+            Toast.makeText(this,"Yup We Can Use 2 Fragments"+mFragment,Toast.LENGTH_LONG).show();
+        }
+        else {
+            mFragment=false;
+            Toast.makeText(this,"No We Can't Use 2 Fragments"+mFragment ,Toast.LENGTH_LONG).show();
+        }
     }
+
 }
