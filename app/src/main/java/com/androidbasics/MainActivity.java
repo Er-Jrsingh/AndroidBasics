a4acd86b67aa93e46e898ce0a2fac856e348dde1
Jitesh Singh <jitust98@gmail.com>
2021-03-01 18:38:50 +0530

    Choosing Layout At Runtime Use Two Fragment or Launch new Activity

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -3,6 +3,7 @@ package com.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
-//    private TextView mShowSize;
+    //    private TextView mShowSize;
 //    private boolean mFragment;
+    boolean mUsingFragment;
+    TextView tv;
 
     @SuppressLint("SetTextI18n")
     @Override
@@ -23,14 +28,39 @@ public class MainActivity extends AppCompatActivity {
 
 //        mShowSize = findViewById(R.id.textView);
 
-        TextView tv = findViewById(R.id.showText);
+        tv = findViewById(R.id.showText);
         ViewGroup viewGroup = findViewById(R.id.details_frag_container);
-        boolean mUsingFragment = (viewGroup != null);
+        mUsingFragment = (viewGroup != null);
         if (mUsingFragment) {
             tv.setText("Using Fragment side by side : True");
         } else {
             tv.setText("Using Fragment side by side : False");
         }
+
+        FloatingActionButton fabB = findViewById(R.id.fab);
+        fabB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDetails();
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void showDetails() {
+
+        if (mUsingFragment) {
+            DetailsFragment detailsFragment = new DetailsFragment();
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.details_frag_container, detailsFragment)
+                    .commit();
+        } else {
+            Intent intent = new Intent(this, DetailsActivity.class);
+            startActivity(intent);
+        }
+
     }
 
 //    public void showSize(View view) {
--- app/src/main/java/com/androidbasics/MenuFragment.java
+++ app/src/main/java/com/androidbasics/MenuFragment.java
@@ -8,11 +8,8 @@ import android.view.ViewGroup;
 
 public class MenuFragment extends Fragment {
 
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_menu, container, false);
     }
