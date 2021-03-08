446f95bea8a9ee9d077f9d4dfa8af4056da445d8
Jitesh Singh <Jitust98@gmail.com>
2021-03-08 14:25:28 +0530

    Show List Of Image&Files Stored in Internal Storage Using Core Java Methods &  Also Delete File

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -2,7 +2,6 @@ package com.androidbasics;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -22,11 +21,10 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
 
     private EditText mFileContent;
-    private Button mBtnCreateFile,mBtnImg,mBtnReadFile,mBtnReadImg;
-    public  static final String FILE_NAME="txt";
-    public  static final String IMG_NAME="assetjitu";
-    private ImageView imageView;
-
+    private Button mBtnCreateFile, mBtnImg, mBtnReadFile, mBtnReadImg, mBtnShowFile, mBtnDeleteText;
+    public static final String FILE_NAME = "txt";
+    public static final String IMG_NAME = "assetjitu";
+    private ImageView mImageView;
 
 
     @Override
@@ -34,50 +32,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String path=getFilesDir().getAbsolutePath();
-        Toast.makeText(this,"Absolute Path : "+path,Toast.LENGTH_LONG).show();
+        String path = getFilesDir().getAbsolutePath();
+        Toast.makeText(this, "Absolute Path : " + path, Toast.LENGTH_LONG).show();
 
-        mFileContent=findViewById(R.id.edt_txt);
-        mBtnCreateFile=findViewById(R.id.btn_create);
-        mBtnImg=findViewById(R.id.btn_get_img);
-        mBtnReadFile=findViewById(R.id.btn_read_file);
-        mBtnReadImg=findViewById(R.id.btn_read_img);
-        imageView=findViewById(R.id.imgView);
+        mFileContent = findViewById(R.id.edt_txt);
+        mBtnCreateFile = findViewById(R.id.btn_create);
+        mBtnImg = findViewById(R.id.btn_get_img);
+        mBtnReadFile = findViewById(R.id.btn_read_file);
+        mBtnReadImg = findViewById(R.id.btn_read_img);
+        mImageView = findViewById(R.id.imgView);
+        mBtnShowFile = findViewById(R.id.btn_list_file);
+        mBtnDeleteText = findViewById(R.id.btn_del_list);
 
         this.mBtnCreateFile.setOnClickListener(this::createFile);
         this.mBtnImg.setOnClickListener(this::writeImg);
         this.mBtnReadFile.setOnClickListener(this::readFile);
         this.mBtnReadImg.setOnClickListener(this::readImg);
+        this.mBtnShowFile.setOnClickListener(this::showFiles);
+        this.mBtnDeleteText.setOnClickListener(this::deleteText);
+    }
+
+    private void showFiles(View view) {
+        String[] fileList = fileList();
+        for (String fileName : fileList) {
+            Toast.makeText(this, fileName, Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private void deleteText(View view) {
+        boolean deleteFile = deleteFile(FILE_NAME);
+        Toast.makeText(this, "File Deleted " + deleteFile, Toast.LENGTH_LONG).show();
     }
 
     private void readImg(View view) {
 
-        Bitmap bitmap =null;
-        InputStream inputStream=null;
+        Bitmap bitmap = null;
+        InputStream inputStream = null;
 
         try {
-            inputStream=openFileInput(IMG_NAME+".jpg");
-            bitmap=BitmapFactory.decodeStream(inputStream);
+            inputStream = openFileInput(IMG_NAME + ".jpg");
+            bitmap = BitmapFactory.decodeStream(inputStream);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
-        imageView.setImageBitmap(bitmap);
+        mImageView.setImageBitmap(bitmap);
     }
 
     private void readFile(View view) {
-        StringBuilder stringBuilder=new StringBuilder();
-        InputStream inputStream=null;
+        StringBuilder stringBuilder = new StringBuilder();
+        InputStream inputStream = null;
 
         try {
-            inputStream=openFileInput(FILE_NAME);
+            inputStream = openFileInput(FILE_NAME);
 
             int read;
-            while ((read=inputStream.read())!=-1){
+            while ((read = inputStream.read()) != -1) {
                 stringBuilder.append((char) read);
             }
 
-            Toast.makeText(this,stringBuilder.toString(),Toast.LENGTH_LONG).show();
+            Toast.makeText(this, stringBuilder.toString(), Toast.LENGTH_LONG).show();
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -87,20 +102,20 @@ public class MainActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     private void writeImg(View view) {
 
-        Bitmap data=getImage();
+        Bitmap data = getImage();
 
 //        BitmapDrawable bitmapDrawable= (BitmapDrawable)getDrawable(R.drawable.assetjitu);
 
-        FileOutputStream outputStream=null;
+        FileOutputStream outputStream = null;
         try {
 //            outputStream = openFileOutput(IMG_NAME+".jpg",MODE_PRIVATE);
-            outputStream = openFileOutput(IMG_NAME+".jpg",MODE_APPEND);
-            data.compress(Bitmap.CompressFormat.JPEG,50,outputStream);
-            Toast.makeText(this,"Write Image Successfully",Toast.LENGTH_LONG).show();
+            outputStream = openFileOutput(IMG_NAME + ".jpg", MODE_APPEND);
+            data.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);
+            Toast.makeText(this, "Write Image Successfully", Toast.LENGTH_LONG).show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } finally {
-            if(outputStream!=null){
+            if (outputStream != null) {
                 try {
                     outputStream.close();
                 } catch (IOException e) {
@@ -112,14 +127,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     private Bitmap getImage() {
 
-        Bitmap img=null;
+        Bitmap img = null;
 
         try {
-            InputStream inputStream=getAssets().open("assetjitu.jpg");
-            img= BitmapFactory.decodeStream(inputStream);
+            InputStream inputStream = getAssets().open("assetjitu.jpg");
+            img = BitmapFactory.decodeStream(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -129,23 +143,22 @@ public class MainActivity extends AppCompatActivity {
 
     private void createFile(View view) {
 
-        String data=mFileContent.getText().toString();
+        String data = mFileContent.getText().toString();
 
-        FileOutputStream outputStream=null;
+        FileOutputStream outputStream = null;
         try {
 //            outputStream = openFileOutput(FILE_NAME,MODE_PRIVATE);
-            outputStream = openFileOutput(FILE_NAME,MODE_APPEND);
+            outputStream = openFileOutput(FILE_NAME, MODE_APPEND);
             outputStream.write(data.getBytes());
             outputStream.flush();
             mFileContent.setText("");
-            Toast.makeText(this,"Write Successfully",Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Write Successfully", Toast.LENGTH_LONG).show();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            if(outputStream!=null){
+        } finally {
+            if (outputStream != null) {
                 try {
                     outputStream.close();
                 } catch (IOException e) {
