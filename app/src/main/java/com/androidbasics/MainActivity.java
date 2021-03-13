d4ac184a99aa03e612affbc9f5c69f0689939a64
Jitesh Singh <Jitust98@gmail.com>
2021-03-13 11:32:50 +0000

    Shared Preference Activity Level(Default Preference Set)

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -1,12 +1,17 @@
 package com.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
+import androidx.appcompat.widget.SwitchCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,12 +20,18 @@ public class MainActivity extends AppCompatActivity {
     public static final String EMAIL_KEY = "email_key";
     public static final String PASS_KEY ="pass_key";
     public static final String FILE_NAME = "my_file";
+    private static final String COLOR_KEY = "color";
+    private SwitchCompat aSwitch;
+    private View mParent;
+
 
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*      Application Level(Named Preference Set)     */
 
         String email=getIntent().getStringExtra(LoginActivity.EMAIL_KEY);
         String pass=getIntent().getStringExtra(LoginActivity.PASS_KEY);
@@ -35,6 +46,27 @@ public class MainActivity extends AppCompatActivity {
         editor.apply();
 
         Toast.makeText(this,"Stored At SharedPreference",Toast.LENGTH_LONG).show();
+
+        /*      Activity Level(Default Preference Set)      */
+
+        aSwitch=findViewById(R.id.switch_btn);
+        mParent = findViewById(R.id.parent_layout);
+        SharedPreferences preferences=getPreferences(MODE_PRIVATE);
+        boolean isChecked=preferences.getBoolean(COLOR_KEY,false);
+        mParent.setBackgroundColor(isChecked?Color.DKGRAY:Color.WHITE);
+        aSwitch.setChecked(isChecked);
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences sharedPreferences=getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor1=sharedPreferences.edit();
+                editor1.putBoolean(COLOR_KEY,isChecked);
+                editor1.apply();
+                mParent.setBackgroundColor(isChecked? Color.DKGRAY:Color.WHITE);
+            }
+        });
+
 
     }
 
