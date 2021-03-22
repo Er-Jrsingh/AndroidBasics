20016adb7eb8dd95ea93c099a9045a058f42a173
Jitesh Singh <Jitust98@gmail.com>
2021-03-22 14:36:39 +0000

    Blocking Call So That UI Thread Cannot Maintain 16fps Refreshing & Flickering Occurs

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -3,12 +3,76 @@ package com.androidbasics;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MyTag";
+    private ScrollView mScrollView;
+    private TextView nLog;
+    private ProgressBar mProgressBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        nLog.setText(R.string.dummy_txt);
+    }
+
+    public void runCode(View view) {
+
+        log("Running Code");
+
+        /*      Blocking Call     */
+
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void log(String message) {
+        Log.i(TAG, message);
+        nLog.append(message + "\n");
+        scrollTextToEnd();
+    }
+
+    public void clearCode(View view) {
+        nLog.setText("");
+        scrollTextToEnd();
+    }
+
+    private void scrollTextToEnd() {
+        mScrollView.post(new Runnable() {
+            @Override
+            public void run() {
+                mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+
+    }
+
+    public void displayProgressBar(boolean display) {
+        if (display) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        } else {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void initView() {
+        mScrollView = findViewById(R.id.scroll_view);
+        nLog = findViewById(R.id.txt_view);
+        mProgressBar = findViewById(R.id.pro_bar);
     }
 }
