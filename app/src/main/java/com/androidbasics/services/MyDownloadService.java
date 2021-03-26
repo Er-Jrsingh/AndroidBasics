ba2e05dc445da1ef992935c3e8ce9a968e537898
Jitesh Singh <Jitust98@gmail.com>
2021-03-26 19:48:08 +0000

    create Worker Thread in Started Service

--- app/src/main/java/com/androidbasics/services/MyDownloadService.java
+++ app/src/main/java/com/androidbasics/services/MyDownloadService.java
@@ -7,7 +7,8 @@ import android.util.Log;
 
 import com.androidbasics.MainActivity;
 
-//    This Is Started Service Because onBind Return Null,By Default It Always Run On UI Thread So That Screen Face Drop Frames(Screen Freeze)
+//    This Is Started Service Because onBind Return Null,By Default It Always Run On UI Thread So That Screen Face Drop Frames(Screen Freeze),After Close App It Still Run On Background
+//     Here We Create Separate Background Thread So Screen Not Blocked,After Close App It Still Run On Background for Always So We Must Have To Write Stop Code
 
 
 public class MyDownloadService extends Service {
@@ -21,10 +22,25 @@ public class MyDownloadService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        Log.d(TAG,"Thread Name : "+Thread.currentThread().getName());
+        Log.d(TAG, "Thread Name : " + Thread.currentThread().getName());
 
-        String songName=intent.getStringExtra(MainActivity.MESSAGE_KEY);
-        downloadSong(songName);
+//        Creating New Background Thread
+
+        final String songName = intent.getStringExtra(MainActivity.MESSAGE_KEY);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                downloadSong(songName);
+
+            }
+        });
+
+        thread.start();
+
+//        String songName=intent.getStringExtra(MainActivity.MESSAGE_KEY);
+//        downloadSong(songName);
 
         return Service.START_REDELIVER_INTENT;
     }
@@ -35,14 +51,14 @@ public class MyDownloadService extends Service {
         return null;
     }
 
-    private void downloadSong(String song){
-       Log.d(TAG,"downloadSong : "+song+" Download Started...");
+    private void downloadSong(String song) {
+        Log.d(TAG, "downloadSong : " + song + " Download Started...");
 
         try {
             Thread.sleep(4000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        Log.d(TAG,"downloadSong : "+song+" Download Finished ");
+        Log.d(TAG, "downloadSong : " + song + " Download Finished ");
     }
 }
