859eb694e27f6132f37fc79e5cede2f3385bb4a2
Jitesh Singh <Jitust98@gmail.com>
2021-04-04 18:48:11 +0000

    Creating User Device Contacts Fetch App To Demonstrate ContentProvider

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -1,14 +1,64 @@
 package com.androidbasics;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+    ArrayList<String> mList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mListView = findViewById(R.id.list_contacts);
+        mList = new ArrayList<>();
+        fetchContacts();
+    }
+
+    private void fetchContacts() {
+//        Ask For Permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, 0);
+        }
+
+        ContentResolver contentResolver = getContentResolver();
+
+        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;       //the content URI of The Contacts,table etc
+        String[] projection = null;                //Use To Access The Column Wise data
+        String selection = null;                    //Use To Access The Row Wise data
+        String[] selectionArgs = null;          //Used When we want To Access Data With Criteria(Where Clause)
+        String order = null;                        //Sort order for the returned rows
+        @SuppressLint("Recycle") Cursor cursor = contentResolver.query(uri, projection, selection, selectionArgs, order);    //This Method Will Return Cursor
+
+        if (cursor.getCount() > 0) {
+            while (cursor.moveToNext()) {
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+                String number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+
+                String completeName = name + "\n" + number;
+
+                mList.add(completeName);        // Cursor Data is copied into ArrayList
+            }
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mList);
+        mListView.setAdapter(adapter);
     }
 }
