8d9b8a1eb18169b3aa2d22a4dbaf6c6ebae14164
Jitesh Singh <Jitust98@gmail.com>
2021-04-09 18:58:09 +0000

    Ordered Broadcast Receiver -Send One Broadcast to Multiple Receivers ,Get And Change Property ,Practical Scenario

--- app/src/main/java/com/androidbasics/MainActivity.java
+++ app/src/main/java/com/androidbasics/MainActivity.java
@@ -1,20 +1,51 @@
 package com.androidbasics;
 
+import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-//        Broadcast Receiver -Send One Broadcast to Multiple Receivers & Set Priority at Receiver( Transfer This Code Into Another Project To Run Both App At a Time ) (Ignore Previous code )
-//       Receiver Code - Ordered Broadcast Receiver -Send One Broadcast to Multiple Receivers ,Get And Change Property
+//        Broadcast Receiver -Send One Broadcast to Multiple Receivers & Set Priority at Receiver
+//       Receiver Code - Ordered Broadcast Receiver -Send One Broadcast to Multiple Receivers ,Get And Change Property ,Practical Scenario (Transfer This Code Into Another Project To Run Both App At a Time ) (Sender Code written In BroadcastReceiver)
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTv;
+    //    This Is Called When App is In Foreground
+    BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            mTv.setText("Congo !! UI Updated");
+            if (isOrderedBroadcast()) {
+                abortBroadcast();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mTv = findViewById(R.id.tv);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter("com.example.custombroadcastsender.ACTION_SEND");
+        intentFilter.setPriority(121);
+        registerReceiver(mReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mReceiver);
     }
 }
--- app/src/main/java/com/androidbasics/OrderedBReceiver1.java
+++ app/src/main/java/com/androidbasics/OrderedBReceiver1.java
@@ -1,13 +1,28 @@
 package com.androidbasics;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.core.app.NotificationCompat;
+
 public class OrderedBReceiver1 extends BroadcastReceiver {
+    //    Called When This App In Background
     @Override
     public void onReceive(Context context, Intent intent) {
+
+        if (isOrderedBroadcast()) {
+            Toast.makeText(context, "BroadcastPracticalScenario Notification", Toast.LENGTH_SHORT).show();
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "ChannelId")
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentText("Notification From BroadcastPracticalScenario")
+                    .setContentTitle("Hola ! Broadcast");
+
+            NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            manager.notify(1, builder.build());
+        }
     }
 }
